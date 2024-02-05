3b18dacd71266d6439f6771082d5263f94ba65a5
jarridkam <jarridaaa@gmail.com>
2024-02-04 20:49:59 -0800

    Mongoose

---
+++ app_server/models/travlr.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: {type: String, requried: true, index: true },
+    length: { type: String, required: true, index: true },
+    start: {type: String, requried: true },
+    resort: { type: String, required: true },
+    perPerson: {type: String, requried: true },
+    image: { type: String, required: true },
+    description: {type: String, requried: true }
+})
