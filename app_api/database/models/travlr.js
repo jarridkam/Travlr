5a762c3398e3330a46d9cb999d34e8a78a98d0a1
jarridkam <jarridaaa@gmail.com>
2024-02-18 18:42:45 -0800

    Fixed backend after git imploded it
    I switched to Module-6 branch in the middle of the project, not knowing the damage it would cause. I re-added all the backend from Module 5.

---
+++ app_api/database/models/travlr.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//define the trip schema
+const tripSchema = new mongoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: {type: String, requried: true, index: true },
+    length: { type: String, required: true, index: true },
+    start: {type: String, requried: true },
+    resort: { type: String, required: true },
+    perPerson: {type: String, requried: true },
+    image: { type: String, required: true },
+    description: {type: String, requried: true }
+});
+
+// mongoose.model("trips", tripSchema):
+module.exports = mongoose.model("trips", tripSchema);
